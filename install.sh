5ac792098ef3fb85eb92fba8fc7eaf1187622190
Psykokwak <alexeivra@gmail.com>
2025-09-23 18:23:20 -0600

    se añade script de instalación

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# # script para instalar lo necesario para XMonad, recordad que esta instalación y configuración está hecha para que corra en mi maquina con OpenSUSE Tumbleweed
+#
+# instalación de herramientas
+sudo zypper install ghc ghc-cabal cabal-install libX11-devel libXext-devel libXft-devel libXrandr-devel libXinerama-devel libXss-devel
+
+# esto instalará XMonad y XMonad-contrib
+cabal install xmonad xmonad-contirb
+
+# las versiones recientes tienen el archivo de configuracion en ~/.config/xmonad, es ahí donde debemos de poner nuestro archivo de configuración del escritorio
+touch ~/.config/xmonad/xmonad.hs
+
+# posteriormente se puede hacer git clone en ~/.config/xmonad o solo mover el archivo xmonad.hs a esta dirección

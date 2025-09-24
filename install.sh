4e026b3be07d40617f5d238e712329e87f3beeea
Psykokwak <alexeivra@gmail.com>
2025-09-23 21:51:45 -0600

    compila, aun no funciona el fondo de pantalla

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 #
 # # script para instalar lo necesario para XMonad, recordad que esta instalación y configuración está hecha para que corra en mi maquina con OpenSUSE Tumbleweed
 #
-# instalación de herramientas
-sudo zypper install ghc ghc-cabal cabal-install libX11-devel libXext-devel libXft-devel libXrandr-devel libXinerama-devel libXss-devel
+# instalación de herramientas (en primera isntancia usaré feh, aunque puedes usar nitrogen u otro gestor de fondos de pantalla)
+sudo zypper install ghc ghc-cabal cabal-install libX11-devel libXext-devel libXft-devel libXrandr-devel libXinerama-devel libXss-devel feh
 
 # esto instalará XMonad y XMonad-contrib
-cabal install xmonad xmonad-contirb
+cabal install xmonad xmonad-contirb --lib # he encontrado que cabal los intenta instalar como software y no como una libreria universal para ghc
 
 # las versiones recientes tienen el archivo de configuracion en ~/.config/xmonad, es ahí donde debemos de poner nuestro archivo de configuración del escritorio
 touch ~/.config/xmonad/xmonad.hs
--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,10 @@
 import XMonad
 
 main :: IO ()
-main = xmonad def
+main = xmonad $ def
+    {modMask = mod4Mask,
+    borderWidth = 2,
+    normalBorderColor = "dddddd",
+    focusedBorderColor = "ff0000",
+    terminal = "kitty" -- esta es la terminal que tengo por defecto, cambiala por la tuya
+    }

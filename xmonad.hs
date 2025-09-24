cd8f8bdc042fc3a09b7e81d872bb93e1cc1b49b8
Psykokwak <alexeivra@gmail.com>
2025-09-24 00:53:44 -0600

    carga el fondo de pantalla (propio) con funcion de xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,10 @@
 import XMonad
+import XMonad.Util.SpawnOnce
+
+-- esta funcion inicia el fondo de pantalla spawnOnce :: String -> X, donde X es una funcion del sistema supongo :v
+myStartupHook :: X ()
+myStartupHook = do
+    spawnOnce "feh --bg-fill /home/psykokwak/Images/fondos/GatoBoca.jpg &"
 
 main :: IO ()
 main = xmonad $ def
@@ -6,5 +12,6 @@ main = xmonad $ def
     borderWidth = 2,
     normalBorderColor = "dddddd",
     focusedBorderColor = "ff0000",
-    terminal = "kitty" -- esta es la terminal que tengo por defecto, cambiala por la tuya
+    terminal = "kitty", -- esta es la terminal que tengo por defecto, cambiala por la tuya
+    startupHook = myStartupHook
     }

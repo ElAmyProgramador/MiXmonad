8c6dc21a2e7aefe46f7d4f61eaff5d17dc5b375c
Psykokwak <alexeivra@gmail.com>
2025-09-26 15:40:25 -0600

    myLayout 'funcional' con gaps

--- lib/MyLayout.hs
+++ lib/MyLayout.hs
@@ -4,12 +4,20 @@ import XMonad
 import XMonad.Layout.Spacing
 import XMonad.Layout.Grid
 import XMonad.Layout.NoBorders
-import XMonad.Layout.LayoutModifier (ModifiedLayout)
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.Gaps
+import XMonad.Hooks.ManageDocks
 
-myLayout :: ModifiedLayout SmartBorder
-             (Choose (ModifiedLayout Spacing Tall)
-                     (Choose (Mirror (ModifiedLayout Spacing Tall))
-                             (Choose Grid Full))) Window
-myLayout = smartBorders (tiled ||| Mirror tiled ||| Grid ||| Full)
+myLayout :: ModifiedLayout AvoidStruts
+               (ModifiedLayout SmartBorder
+                   (ModifiedLayout Gaps
+                       (Choose (ModifiedLayout Spacing Tall)
+                               (Choose (Mirror (ModifiedLayout Spacing Tall))
+                                       (Choose Grid Full)))))
+               Window
+myLayout = avoidStruts $
+             smartBorders $
+               gaps [(U,20), (D,8), (L,8), (R,8)] $
+                 (tiled ||| Mirror tiled ||| Grid ||| Full)
   where
     tiled = spacing 8 $ Tall 1 (3/100) (1/2)
